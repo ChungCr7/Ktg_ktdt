316bae2e68ad5b91ff2dc6b16c1eca5dae9cd91b
ChungCr7 <baochungas3@gmail.com>
2024-11-17 22:12:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const processDatabase = async () => {
 };
 
 // Cổng
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server đang chạy trên cổng ${PORT}`);
 });
